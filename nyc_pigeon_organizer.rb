397373c1ccb89b3a7299eb198780ec3b26b669b9
Emily R <emilyroulund@gmail.com>
2019-05-10 21:37:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,16 +2,18 @@ def nyc_pigeon_organizer(data)
   new_hash = {} 
   data.each do |descriptors, specifics|
     specifics.each do |describe, array|
-      array.each do |names|
-        if  new_hash.has_key?(names)
-          if new_hash.has_key?(descriptors)
-            new_hash[names][descriptors] << describe
+      array.each do |name|
+        if  new_hash.has_key?(name)
+          if new_hash[name].has_key?(descriptors)
+            new_hash[name][descriptors] << describe.to_s
           else 
             new_hash[name][descriptors] = [describe.to_s]
           end
-        new_hash[name] = {key => [describe.to_s]}
+        else
+        new_hash[name] = {descriptors => [describe.to_s]}
         end
       end 
+    end
   end
   new_hash
 end
